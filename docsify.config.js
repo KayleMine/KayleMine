2efdafbf3c683b2bb8022448450502128f7636d4
KayleMine <60858771+KayleMine@users.noreply.github.com>
2023-11-19 18:52:01 +0600

    Add files via upload

---
+++ docsify.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  main: {
+    name: 'Collman fruits example',
+    basePath: 'https://raw.githubusercontent.com/KayleMine/KayleMine/main/docs/',
+    repo: '',
+    loadSidebar: true,
+    alias: {
+      '/.*/_sidebar.md': '/_sidebar.md'
+    },
+    subMaxLevel: 3,
+    sidebarDisplayLevel: 1,
+    search: 'auto'
+  },
+}
